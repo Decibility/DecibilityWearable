7e0cd4b78a36d2740383ceecd13e6b3b3d708c2d
Matthew Tave <tavematthew@gmail.com>
2024-01-21 12:39:25 -0500

    Migrated to C++
    migrated project to C++ to use bluetooth library

--- main/main.cpp
+++ main/main.cpp
@@ -11,8 +11,23 @@
 
 const char *TAG = "DecibilityWearable";
 
-void app_main(void)
+// Check if Bluetooth is available
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+// Check Serial Port Profile
+#if !defined(CONFIG_BT_SPP_ENABLED)
+#error Serial Port Profile for Bluetooth is not available or not enabled. It is only available for the ESP32 chip.
+#endif
+
+#include "BluetoothSerial.h"
+BluetoothSerial SerialBT;
+
+extern "C" void app_main()
 {
+    SerialBT.begin("Decibility ESP-32");
+
     // Sets the priority of app_main to be higher than the threads being added
     // This makes sure app_main completed adding threads before any threads are started
     vTaskPrioritySet(NULL, tskIDLE_PRIORITY + 3);
